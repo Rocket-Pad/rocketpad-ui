a808f9438546fc62e46d5e911c8010d6f1e1298d
mumeen <azeezmumeen6@gmail.com>
2023-08-04 10:14:48 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return <div className="text-6xl">WELCOME TO ROCKETPAD</div>;
+}
+
+export default App;
