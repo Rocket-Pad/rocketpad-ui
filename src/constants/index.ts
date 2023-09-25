958aa24bb386b93450720339407fde999aae1a1e
mumeen <azeezmumeen6@gmail.com>
2023-09-25 22:46:06 +0100

    add backend url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,10 @@
 export const defaultChainId = 155;
+const LIVE = import.meta.env.VITE_APP_LIVE === "true";
 
 export const factoryAddresses = {
   155: "0x4f508c490E805cfd07342041F8A407f204Efc10c",
 };
 
-export const API_ENDPOINT = "http://localhost:8000/";
+export const API_ENDPOINT = LIVE
+  ? "https://api-b71l.onrender.com/"
+  : "http://localhost:8000/";
